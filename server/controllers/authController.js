68cee2f8b620de92099dd74aa2d5156173e155bf
noorjsdivs <noor.jsdivs@gmail.com>
2025-07-18 00:34:25 +0600

    part 2 done

---
+++ server/controllers/authController.js
@@ -0,0 +1,12 @@
+import asyncHandler from "express-async-handler";
+const loginUser = (req, res) => {
+  res.send("Login Controller is working");
+};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, role } = req.body;
+  console.log(name, email, password, role);
+
+  res.send("Register is working");
+});
+
+export { loginUser, registerUser };
